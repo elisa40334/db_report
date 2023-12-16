0c2dbf0a0719357fded18954cf19173303374fbe
April-ying <april910909@gmail.com>
2023-12-16 18:18:10 +0800

    編輯員工(完成)

--- editEmployee.php
+++ editEmployee.php
@@ -6,21 +6,15 @@
     $position = @$_POST['position'];
     $salary = @$_POST['salary'];
     $DName = @$_POST['DName'];
-    echo "用POST測試將HTML內容傳入PHP:<br>";
-    echo "今天的日期為".$EName;
-    echo $EId;
+    
     require_once 'dbconnect.php';
-
-    $URL = $_SERVER['REQUEST_URI'];
+    $URL = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'No referer';
     $parts = explode('=', $URL);
     $part = urldecode($parts[1]);//取網址中的EName
-    // sql語法存在變數中
-    $query = "SELECT *FROM employee WHERE EName = '$part'"; //搜尋*(全部欄位) ，從表employee
-    // 用mysqli_query方法執行(sql語法)將結果存在變數中
-    $result = mysqli_query($link,$query);
 
-    $query = "UPDATE 'employee' SET EId = '$EId' WHERE EName = '$part'";
-    
-    $result = mysqli_query($link,$query);
-    
+    $sql = "UPDATE employee SET EId='$EId',EName='$EName' ,EPhone='$EPhone', address='$address',position='$position',salary='$salary',DName='$DName' WHERE EName='$part'";
+    mysqli_query($link, $sql);
+
+    mysqli_close($link);
+    header("Refresh:0; employee.php?employee=". urlencode($EName));
 ?>
