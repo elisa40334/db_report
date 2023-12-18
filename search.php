3027137c2009d93557ce5b0bd8ae034d1cd1ba75
AnitaX1018 <yujing168100@gmail.com>
2023-12-18 19:12:14 +0800

    fix: 少一個括號

--- search.php
+++ search.php
@@ -20,7 +20,7 @@
             echo "<div id='department-name' style='font-style:italic; font-size: 40px; color: white;'>以下是‘" . $part . "’的搜尋結果</div>";
             ?>
         </div>
-        
+
         <!--hover會跑掉算了css真的好難QQ-->
 
     </header>
@@ -30,32 +30,32 @@
         <div id="department-name"></div>
 
         <?php
-            $URL = $_SERVER['REQUEST_URI'];
-            $parts = explode('?', $URL);
-            $parts = explode('=', $parts[1]);
-            $part = urldecode($parts[1]);
-            $part = iconv('UTF-8', 'ISO-8859-1//IGNORE', $part);
-            // 載入db.php來連結資料庫
-            if($part){
-                require_once 'dbconnect.php';
-                // 設置一個空陣列來放資料
-                $datas = array();
-                // echo $part;
-                $sql = "SELECT * FROM employee WHERE EName LIKE '%$part%'";
-                $result = mysqli_query($link,$sql);
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<br><div id='department-result'><a class='dropdown-item' href='employee.php?employee=" . $row["EName"] . "'>" . $row["EName"] . " 職位: " . $row["position"] . " " . " 電話: " . $row["EPhone"] . "</a><br>";
-                    }
-                } else {
-                    echo "<br><div id='department-result' dropdown-item'>暫無資料<br>";
-
+        $URL = $_SERVER['REQUEST_URI'];
+        $parts = explode('?', $URL);
+        $parts = explode('=', $parts[1]);
+        $part = urldecode($parts[1]);
+        $part = iconv('UTF-8', 'ISO-8859-1//IGNORE', $part);
+        // 載入db.php來連結資料庫
+        if ($part) {
+            require_once 'dbconnect.php';
+            // 設置一個空陣列來放資料
+            $datas = array();
+            // echo $part;
+            $sql = "SELECT * FROM employee WHERE EName LIKE '%$part%'";
+            $result = mysqli_query($link, $sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "<br><div id='department-result'><a class='dropdown-item' href='employee.php?employee=" . $row["EName"] . "'>" . $row["EName"] . " 職位: " . $row["position"] . " " . " 電話: " . $row["EPhone"] . "</a><br>";
+                }
             } else {
-                echo "<br><div id='department-result'>暫無資料<br>";
+                echo "<br><div id='department-result' dropdown-item'>暫無資料<br>";
             }
+        } else {
+            echo "<br><div id='department-result'>暫無資料<br>";
+        }
 
-            
+
         ?>
 
     </main>
