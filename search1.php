a0678feb6a810f6afd2be24af13d233d216ee340
AnitaX1018 <yujing168100@gmail.com>
2023-12-19 18:04:05 +0800

    fix: search css

--- search1.php
+++ search1.php
@@ -27,6 +27,7 @@
     </header>
 
     <main>
+    </main>
         <!--抓單位名字放最上面-->
         <div id="department-name"></div>
 
@@ -72,7 +73,7 @@
 
         ?>
 
-    </main>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
