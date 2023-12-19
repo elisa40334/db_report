ecbf9287924e344274d6768a4f128d2c8b85d081
April-ying <april910909@gmail.com>
2023-12-19 21:06:54 +0800

    fix:employee

--- editEmployee.php
+++ editEmployee.php
@@ -15,7 +15,8 @@
 
     $sql = "UPDATE employee SET EId='$EId',EName='$EName' ,EPhone='$EPhone', address='$address',position='$position',salary='$salary',DName='$DName',photo_link='$photo_link' WHERE EName='$part'";
     $result=mysqli_query($link, $sql);
-    if($result){echo "成功";}
+    if($result){echo "編輯成功";}
+    else{echo "編輯失敗";}
 
     mysqli_close($link);
     header("Refresh:0; employee.php?employee=". urlencode($EName));
